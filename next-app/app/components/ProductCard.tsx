68f0e937c88c7de01628db37b03e34962428ed29
Lydia Bandy <121919536+LydiasPianoStudio@users.noreply.github.com>
2024-09-17 18:51:29 -0400

    installed daisyui and used tailwind

--- next-app/app/components/ProductCard.tsx
+++ next-app/app/components/ProductCard.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import AddToCart from "./AddToCart";
+//import styles from "./ProductCard.module.css";
 
 const ProductCard = () => {
   return (
-    <div>
+    <div className="p-5 my-5 bg-amber-400 text-white text-xl hover:bg-amber-500">
       <AddToCart />
     </div>
   );
