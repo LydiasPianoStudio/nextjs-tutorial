1537a0fde222cace7d2c145e67494bff6be1983e
Lydia Bandy <121919536+LydiasPianoStudio@users.noreply.github.com>
2024-09-17 19:12:01 -0400

    finished tutorial

--- next-app/app/layout.tsx
+++ next-app/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" data-theme="retro">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
--- next-app/tailwind.config.ts
+++ next-app/tailwind.config.ts
@@ -15,6 +15,9 @@ const config: Config = {
     },
   },
   plugins: [require('daisyui')],
+  daisyui: {
+    themes: ["retro"],
+  },
       
     
 };
